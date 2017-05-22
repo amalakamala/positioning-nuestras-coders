95f3d207a7de8d013268c78dd511eeb1d9506a70
amalakamala <amalakamala.a@gmail.com>
2017-05-22 18:29:32 -0400

    las codeers

---
+++ js/main.js
@@ -0,0 +1,72 @@
+var titulo = document.getElementsByClassName("titulo")[0];
+var linea = document.getElementsByClassName("linea")[0];
+
+
+titulo.style.margin = "20px";
+titulo.style.position = "relative"; 
+titulo.style.left = "40px";
+
+
+linea.style.width = "70px";
+linea.style.height = "5px";
+linea.style.background = "#000601";
+linea.style.position = "absolute"; 
+linea.style.top = "45px";
+linea.style.left = "68px";
+
+var uno = document.getElementsByClassName("alumna-1")[0];
+var dos = document.getElementsByClassName("alumna-2")[0];
+var tres = document.getElementsByClassName("alumna-3")[0];
+var cuatro = document.getElementsByClassName("alumna-4")[0];
+var cinco = document.getElementsByClassName("alumna-5")[0];
+var seis = document.getElementsByClassName("alumna-6")[0];
+
+uno.style.width = "211px";
+uno.style.height = "211px";
+uno.style.position = "absolute"; 
+uno.style.top = "80px";
+uno.style.left = "68px";
+
+
+dos.style.width = "211px";
+dos.style.height = "211px";
+dos.style.position = "absolute"; 
+dos.style.top = "80px";
+dos.style.left = "368px";
+
+tres.style.width = "211px";
+tres.style.height = "211px";
+tres.style.position = "absolute"; 
+tres.style.top = "80px";
+tres.style.left = "668px";
+
+cuatro.style.width = "211px";
+cuatro.style.height = "211px";
+cuatro.style.position = "absolute"; 
+cuatro.style.top = "380px";
+cuatro.style.left = "68px";
+
+cinco.style.width = "211px";
+cinco.style.height = "211px";
+cinco.style.position = "absolute";
+cinco.style.top = "380px";
+cinco.style.left = "368px"; 
+
+seis.style.width = "211px";
+seis.style.height = "211px";
+seis.style.position = "absolute"; 
+seis.style.top = "380px";
+seis.style.left = "668px";
+
+
+var span = Array.from(document.getElementsByClassName("nombre"));
+
+span.forEach(function(e){
+	e.style.position = "absolute"; 
+	e.style.right = "5px";
+	e.style.bottom = "5px";
+	e.style.color = "white";
+});
+
+
+
